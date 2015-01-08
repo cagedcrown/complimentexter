a110aeeb1cbd4bb49a622045c6ea6fe0d414b415
Maury Lindo <maury@cagedcrown.com>
2015-01-07 20:35:04 -0500

    finished homework

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,11 +1,43 @@
 class NotificationsController < ApplicationController
-
-  def create
+	def index
+		@notification = Notification.new
+	end
+	def create
+		@notification = Notification.new
+		@notification.text
+		render :index
     #add the code that will create a new notification and text it to the provided number 
-  end
-
-  private
-  def notification_params
-    #what are the params needed to make a new notification? (refer to your model)
-  end
 end
+
+private
+def notification_params
+
+    #what are the params needed to make a new notification? (refer to your model)
+end
+end
+# class NotificationsController < ApplicationController
+# 	def index
+# 		@notification = Notification.new
+# 	end
+
+# 	def create
+# 		@notifaction = Notification.new
+# 		@notification.text
+# 		render :index
+#     #add the code that will create a new notification and text it to the provided number
+#     # @client.messages.create(
+#     # 	from: '+12402057721',
+#     # 	to: '+13019191674',
+#     # 	body: 'Hey there!'
+#     # 	)
+
+
+# 	def text
+#     	# @text = "This is a random sentence."
+#     end
+# end
+#     private
+#     def notification_params
+# 	    #what are the params needed to make a new notification? (refer to your model)
+# 	end
+# end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,17 +5,24 @@ class Notification
 
   def initialize(options={})
     self.to_number = options[:to_number]
-    account_sid = #ENV["twilio_sid"]
-    auth_token =  #ENV["twilio_token"]
+    account_sid = ENV["TWILIO_SID"]
+    auth_token =  ENV["TWILIO_TOKEN"]
     @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
   def text
-    # Sends a text to your phone number
+    # @text = "This is a random sentence."
+    #add the code that will create a new notification and text it to the provided number
+    @client.messages.create(
+      from: '+12402057721',
+      to: '+13019191674',
+      body: random_compliment
+      )
   end
 
   def random_compliment
-    compliments = [] #enter compliments there
+    random_word = ["Cool", "Awesome", "Nice", "Fun", "Okay", "Not bad", "Suck", "Terrible", "Fantastic", "Crazy"]
+    "You're #{random_word.sample.downcase}!" #enter compliments there
     # Generates a random compliment to send to the textee
   end
 end
